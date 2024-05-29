5127c6ee49eede78df1b4ea5d6eedad3b4dac222
Yasser <yasserdalouzi@gmail.com>
2024-05-29 12:25:27 +0100

    Update 0-fizzbuzz.py

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -15,17 +15,16 @@ def fizzbuzz(n):
     if n < 1:
         return
 
-   tmp_result = []
+    tmp_result = []
     for i in range(1, n + 1):
-        if (i % 3) == 0 and (i % 5) == 0:  # Check for multi of 3 and 5
-            tmp_result.append("FizzBuzz")
-        elif (i % 3) == 0:  # check for multi of 3
+        if (i % 3 == 0) and (i % 5 != 0):
             tmp_result.append("Fizz")
-        elif (i % 5) == 0:  # check for multi of 5
+        elif (i % 3) == 0 and (i % 5) == 0:
+            tmp_result.append("FizzBuzz")
+        elif (i % 5 == 0) and (i % 3 != 0):
             tmp_result.append("Buzz")
         else:
-            tmp_result.append(str(i))  # Default case
-
+            tmp_result.append(str(i))
     print(" ".join(tmp_result))
 
 
